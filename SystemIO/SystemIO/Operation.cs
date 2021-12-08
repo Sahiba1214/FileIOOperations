7445b596330ec50fe875477d433d918f0ea7181a
sahiba <sahiba1214@gmail.com>
2021-12-09 00:11:27 +0530

    [File  added IO Operations Done]

---
+++ SystemIO/SystemIO/Operation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace SystemIO
+{
+    public class Operations
+    {
+        public void FileExists()
+        {
+            string path = @"F:\IO\FileIOOperations\SystemIO\SystemIO\Example.txt";
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File Exists");
+            }
+            Console.WriteLine();
+        }
+        public void ReadAllLines()
+        {
+            string path = @"F:\IO\FileIOOperations\SystemIO\SystemIO\Example.txt";
+            string[] lines;
+            lines = File.ReadAllLines(path);
+            Console.WriteLine(lines[0]);
+            Console.WriteLine(lines[1]);
+            Console.WriteLine();
+        }
+        public void ReadAllText()
+        {
+            string path = @"F:\IO\FileIOOperations\SystemIO\SystemIO\Example.txt";
+            string lines;
+            lines = File.ReadAllText(path);
+            Console.WriteLine(lines);
+            Console.WriteLine();
+        }
+        public void FileCopy()
+        {
+            string path = @"F:\IO\FileIOOperations\SystemIO\SystemIO\Example.txt";
+            string copyPath = @"F:\IO\FileIOOperations\SystemIO\SystemIO\NewExample.txt";
+            File.Copy(path, copyPath);
+            Console.WriteLine();
+        }
+        public void DeleteFile()
+        {
+            string path = @"F:\IO\FileIOOperations\SystemIO\SystemIO\Example.txt";
+            File.Delete(path);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ SystemIO/SystemIO/StreamReaderWriter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace SystemIO
+{
+    public class StreamReaderWriter
+    {
+        public void WriteUsingStreamReader()
+        {
+            string path = @"F:\IO\FileIOOperations\SystemIO\SystemIO\Example.txt";
+            using (StreamWriter sr = File.AppendText(path))
+            {
+                sr.WriteLine(" Hello World - .NET is Awesome");
+                sr.Close();
+                Console.WriteLine(File.ReadAllText(path));
+            }
+            Console.WriteLine();
+        }
+        public void ReadFromStreamReader()
+        {
+            string path = @"F:\IO\FileIOOperations\SystemIO\SystemIO\Example.txt";
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
